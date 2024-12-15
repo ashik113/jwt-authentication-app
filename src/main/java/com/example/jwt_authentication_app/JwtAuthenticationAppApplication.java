c4db3b823feec774ce919d4dc1a51f6c46a7a1e3
ashik113 <ashikcsbtech@gmail.com>
2024-12-15 10:35:08 +0530

    base spring boot project code

---
+++ src/main/java/com/example/jwt_authentication_app/JwtAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_authentication_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt_authentication_app/JwtAuthenticationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_authentication_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
